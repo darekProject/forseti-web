f691a72b51c12d8362d4b4ce6196924c134b526c
darekSkiciak <dariusz.skiciak@gmail.com>
2018-03-28 21:23:12 +0200

    feat: - add css to header

--- src/comonents/Header/Header.js
+++ src/comonents/Header/Header.js
@@ -1,6 +1,8 @@
 import React, {Component, Fragment} from 'react';
 import {Link} from 'react-router-dom';
 
+import './Header.css';
+
 class Header extends Component {
 
     renderLinks = () => {
@@ -16,28 +18,30 @@ class Header extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <a className="navbar-brand" href="#">FORSETI</a>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                    </ul>
-                    <div className="navbar-collapse collapse">
-                        <ul className="nav navbar-nav ml-auto">
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button"
-                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i className="fas fa-user"></i>
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    {this.renderLinks()}
-                                </div>
-                            </li>
+            <nav className="navbar navbar-toggleable-md">
+                <div className="container">
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <a className="navbar-brand" href="#">FORSETI</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
                         </ul>
+                        <div className="navbar-collapse collapse">
+                            <ul className="nav navbar-nav ml-auto">
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button"
+                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i className="fas fa-user"></i>
+                                    </a>
+                                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        {this.renderLinks()}
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
